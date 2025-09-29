a3bd84fa94604d72ebbd7baaf7310676b37c77cf
somiljain2006 <somil16022006@gmail.com>
2025-09-30 03:41:42 +0530

    Implemented constructors, getters and setters

--- src/main/java/com/example/demo/Homepage.java
+++ src/main/java/com/example/demo/Homepage.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.content.Description;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +17,9 @@ public class Homepage {
 	}
 
     @GetMapping
-    public String hello() {
-        return "Hello World!";
+    public List<Description> hello() {
+        return List.of(
+                new Description(1L, "My name is Somil")
+        );
     }
 }
